17c7121b904f950c7d9dcfcd47bd98ce5c864e40
FaresLevi <121386851+FaresLevi@users.noreply.github.com>
2024-07-13 23:29:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+asideDiv = document.getElementById("asideDiv");
+rightSection = document.querySelector(".rightSection");
+
+
+
+function opening(){
+    asideDiv.style.display = "block";
+    asideDiv.style.background = "rgba(0, 0, 0, 0.5)"
+}
+
+function closing(){
+    asideDiv.style.display = "none";
+}
+
+
